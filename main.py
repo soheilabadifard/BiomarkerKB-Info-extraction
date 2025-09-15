f359c645029093cb962d4228c77150e108c0a767
Soheil <abadifardso@gmail.com>
2025-09-15 14:37:47 -0500

    temp

--- main.py
+++
@@ -1,4 +0,0 @@
-import requests, json
-spec = requests.get("https://api.biomarkerkb.org/swagger.json", timeout=30).json()
-paths = spec.get("paths", {})
-print(json.dumps(sorted(paths.keys()), indent=2))
